66392928ea636b65594c753dc1e30f6f72f38b62
kanistan123 <kanijegatheesar9@gmail.com>
2026-04-01 13:31:35 +0530

    second commit

--- src/test/java/test/basic_test.java
+++ src/test/java/test/basic_test.java
@@ -2,7 +2,7 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
-import com.microsoft.playwright.Locator;
+
 
 import base.basetest;
 
@@ -11,13 +11,7 @@ public class basic_test extends basetest {
 	@Test
 	public void test1() {
 
-		Locator email = page.locator("id=username");
-		email.fill("superadmin");
-
-		Locator password = page.locator("//input[@type='password']");
-		password.fill("superadmin");
-
-		page.locator("//span[text()='Sign in']").click();
+		
 	}
 
 }
