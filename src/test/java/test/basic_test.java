231a33de8d167232635a7b6a171f5afe3c4c0206
kanistan123 <kanijegatheesar9@gmail.com>
2026-04-01 07:33:12 +0530

    initial to my branch

--- src/test/java/test/basic_test.java
+++ src/test/java/test/basic_test.java
@@ -2,39 +2,22 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
 
-public class basic_test {
+import base.basetest;
 
-	
+public class basic_test extends basetest {
+
 	@Test
 	public void test1() {
-		Playwright playwright = Playwright.create();
-		Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		Page page=browser.newPage();
-		page.navigate("https://bms.invictainnovations.com/admin/organization");
-		
-		Locator email=page.locator("id=username");
+
+		Locator email = page.locator("id=username");
 		email.fill("superadmin");
-		
-		Locator password=page.locator("//input[@type='password']");
+
+		Locator password = page.locator("//input[@type='password']");
 		password.fill("superadmin");
-		
+
 		page.locator("//span[text()='Sign in']").click();
-		
-		page.close();
-		
-		
-		
-		
-		
 	}
-	
-	
-	
-	
+
 }
